5403c0b300008120f43ff138d04dab15459a3452
amykangweb <selene6023@gmail.com>
2016-01-23 09:00:18 -0800

    chapter 3

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, char** argv) {
   puts("Hello, world!");
+
+  int add_together(int x, int y) {
+    int result = x + y;
+    return result;
+  }
+
+  int added = add_together(10, 18);
+  printf("%d\n", added);
+
+  typedef struct {
+    float x;
+    float y;
+  } point;
+
+  point p;
+  p.x = 0.1;
+  p.y = 10.0;
+
+  float length = sqrt(p.x * p.x + p.y * p.y);
+
+  printf("%.6f\n", length);
+
   return 0;
 }
